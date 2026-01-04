fa950be6ee5a3df4d4befb7803a97ec51aca3894
Roman <roman@rospies.dev>
2026-01-04 15:19:56 +0000

    Initial commit

---
+++ zz_gcc15_ar_ranlib.cmake
@@ -0,0 +1,3 @@
+set(CMAKE_AR "/usr/local/bin/gcc-ar" CACHE STRING "" FORCE)
+set(CMAKE_RANLIB "/usr/local/bin/gcc-ranlib" CACHE STRING "" FORCE)
+set(CMAKE_NM "/usr/local/bin/gcc-nm" CACHE STRING "" FORCE)
